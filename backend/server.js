52524c32dd342d32603c26b2d7fdea172363589b
Ellijah Jimenez <ellijah_jimenez@dlsu.edu.ph>
2021-04-26 02:36:37 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,14 @@ app.use(express.json());
 mongoose.connect("mongodb+srv://admin:1234@maincluster.3efyv.mongodb.net/Vi2DB?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
-  
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+  res.sendFile(path.join(_dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.listen(process.env.PORT || 5000,()=>{
     console.log ( "Server Has Started" );
 })
