1d41b6900e548d0196def99e0fef6d368e42ec6b
Ellijah Jimenez <ellijah_jimenez@dlsu.edu.ph>
2021-04-26 02:59:23 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 app.use(express.json());
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(_dirname, 'client', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
 app.post('/api/dialogflow/textQuery',async (req, res)=>{
