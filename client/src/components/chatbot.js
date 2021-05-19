a66edd12d28f02d48b12e631c84786c3de383b42
Darren Tee <45459431+darrenrtee@users.noreply.github.com>
2021-05-20 02:34:40 +0800

    Update chatbot.js

--- client/src/components/chatbot.js
+++ client/src/components/chatbot.js
@@ -102,7 +102,7 @@ class chatbot extends Component {
             this.props.setQuestionType(response.data.outputContexts[0].parameters.fields.questiontype.stringValue)
             this.props.addMessage(message2)
           }
-          else if(content.text.text[0] == "Congratulations! You got the correct answer!"){
+          else if(content.text.text[0] === "Congratulations! You solved the problem!"){
             this.props.addMessage(message2)
             
             const response1 = await Axios.post('/api/dialogflow/textQuery',{"text":"yes"})
