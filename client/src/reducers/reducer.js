d5ba06b63c134c7e0df50fedafe45958805aa47c
beaaaar97 <47263246+beaaaar97@users.noreply.github.com>
2021-11-21 19:55:35 +0800

    Update reducer.js

--- client/src/reducers/reducer.js
+++ client/src/reducers/reducer.js
@@ -14,7 +14,7 @@ const initialState = {
     
 
     //definition of problem variables
-    problem: "Talk to Vi2 at the chatbox on the lower right of the screen to start!",
+    problem: "Talk to Vi2 at the chatbox below",
     inventoryOneName: "",
     inventoryTwoName: "",
     itemName: "",
